d45e8587eccd08d8090f9cb00a9f84c22f9077c9
danielisaksson <isak@northkingdom.com>
2019-12-08 18:50:38 +0100

    fixed double import of contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import path from 'path'
 import dotenv from 'dotenv'
 import * as contentful from 'contentful'
-// import contentful from 'contentful'
 dotenv.config()
 
 /* const fs = require('fs')
